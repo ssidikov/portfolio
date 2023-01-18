00f8bb6bef41d75972877c7ab11872b25c4a56be
Sardorbek Sidikov <sardorbek.sidikov7@gmail.com>
2023-01-18 16:33:37 +0100

    changes

--- Script/script.js
+++
@@ -1,9 +0,0 @@
-let profileElement = document.querySelector(".profile");
-let buttomThemeChange = document.querySelector("#js-change-theme-button");
-
-buttomThemeChange.addEventListener("click", function () {
-  profileElement.classList.toggle("dark-mode");
-  buttomThemeChange.innerHTML === "☀️"
-    ? (buttomThemeChange.innerHTML = "🌙")
-    : (buttomThemeChange.innerHTML = "☀️");
-});
