64b6893f86fda1a6dc03252e3e24b55af47bf6d1
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2025-01-26 23:46:28 +0100

    Refactor Header component to improve button styles and layout consistency

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
 import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import { Moon, Sun } from 'lucide-react'
@@ -12,24 +11,23 @@ export default function Header() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <header className='fixed top-0 left-0 right-0 z-50 bg-background/70 backdrop-blur-md border-b border-background-100 container mx-auto px-4 py-3 flex items-center justify-between'>
+    <header className='fixed top-0 left-0 right-0 z-50 bg-background/70 backdrop-blur-md border-b border-background-100 container mx-auto px-4 py-2 flex items-center justify-between'>
       <Link href='/' className='flex items-center gap-2'>
-        <Image src='/logo.svg' alt='Logo' layout='intrinsic' width={200} height={100} />
+        <Image src='/logo.svg' alt='Logo' width={200} height={100} />
       </Link>
 
       <div className='flex items-center md:hidden'>
-        <Button
-          variant='outline'
-          size='icon'
+        <button
           onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-          className='mr-2 bg-background/80 backdrop-blur-md border-b border-background-100'>
-          <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          className='border mr-2 p-2 rounded-md bg-background/80 text-gray-800 dark:text-gray-200'
+          aria-label='Toggle theme'>
+          <Sun className='absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <Moon className='relative h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
-        </Button>
+        </button>
         <button
           onClick={() => setMenuOpen(!menuOpen)}
-          className='flex items-center justify-center w-10 h-10 rounded focus:outline-none focus:ring-2 focus:ring-primary'
+          className='flex items-center justify-center w-10 h-10 rounded-md focus:outline-none focus:ring-2'
           aria-label='Toggle menu'>
           <svg
             className={`w-6 h-6 transition-transform ${menuOpen ? 'rotate-90' : ''}`}
@@ -51,22 +49,22 @@ export default function Header() {
       <nav
         className={`${
           menuOpen ? 'block' : 'hidden'
-        } absolute top-16 left-0 w-full md:static md:flex md:gap-6 md:items-center md:justify-center md:mt-0 pb-4 md:pb-0 mt-4 md:flex-row bg-background backdrop-blur-md md:bg-transparent md:backdrop-blur-none`}>
+        } absolute top-16 left-0 w-full md:static md:flex md:w-auto  md:mt-0 pb-4 md:pb-0 mt-4 md:flex-row md:gap-6 md:items-center md:justify-center bg-background md:bg-transparent`}>
         <Link
           href='/#home'
-          className='block px-4 py-2 text-sm hover:text-primary transition-colors md:inline text-gray-600 dark:text-gray-300'
+          className='block px-4 py-2 text-sm hover:text-primary transition-colors md:inline text-gray-600 dark:text-gray-300 dark:hover:text-blue-500'
           onClick={() => setMenuOpen(false)}>
           Home
         </Link>
         <Link
           href='/#portfolio'
-          className='block px-4 py-2 text-sm hover:text-primary transition-colors md:inline text-gray-600 dark:text-gray-300'
+          className='block px-4 py-2 text-sm hover:text-primary transition-colors md:inline text-gray-600 dark:text-gray-300 dark:hover:text-blue-500'
           onClick={() => setMenuOpen(false)}>
           Portfolio
         </Link>
         <Link
           href='/#about'
-          className='block px-4 py-2 text-sm hover:text-primary transition-colors md:inline text-gray-600 dark:text-gray-300'
+          className='block px-4 py-2 text-sm hover:text-primary transition-colors md:inline text-gray-600 dark:text-gray-300 dark:hover:text-blue-500'
           onClick={() => setMenuOpen(false)}>
           About me
         </Link>
@@ -77,31 +75,28 @@ export default function Header() {
             href='/#contact'
             className='block px-4 py-2 text-sm hover:text-primary transition-colors md:inline text-gray-600 dark:text-gray-300 mt-4 md:mt-0'
             onClick={() => setMenuOpen(false)}>
-            <Button variant='outline' className='w-full md:w-auto'>
+            <button className='w-full md:w-auto px-4 py-2 text-sm font-medium bg-transparent border rounded-md  hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
               Contact Me
-            </Button>
+            </button>
           </Link>
         )}
       </nav>
 
       {/* Contact and dark theme on large screens */}
-      <div className='hidden md:flex items-center gap-4 '>
+      <div className='hidden md:flex items-center gap-4'>
         <Link href='/#contact'>
-          <Button
-            variant='outline'
-            className='bg-background/80 backdrop-blur-md border-b border-background-100'>
+          <button className='px-4 py-2 text-sm font-medium bg-transparent border rounded-md  hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
             Contact Me
-          </Button>
+          </button>
         </Link>
-        <Button
-          variant='outline'
-          size='icon'
+        <button
           onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-          className='bg-background/80 backdrop-blur-md border-b border-background-100'>
-          <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          className='border mr-2 p-2 rounded-md bg-background/80 text-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800'
+          aria-label='Toggle theme'>
+          <Sun className='absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <Moon className='relative h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
-        </Button>
+        </button>
       </div>
     </header>
   )
