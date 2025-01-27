2ff93681a51555e1b00bbe126ade5418a4dfd596
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2025-01-27 08:24:01 +0100

    Refactor Technologies component to remove framer-motion and improve layout structure

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
 
 const technologies = [
   { name: 'React', icon: '/logo/react.svg' },
@@ -21,24 +20,19 @@ export default function Technologies() {
   return (
     <section className='container mx-auto px-4 py-20'>
       <div className='grid grid-cols-4 md:grid-cols-6 gap-[1px] border border-gray-800 bg-gray-800'>
-        {/* Заголовок */}
-        <div className='col-span-4 md:col-span-2 bg-background p-8 min-h-[120px] flex items-center border'>
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}>
-            <h2 className='text-2xl md:text-3xl font-bold leading-tight text-primary'>
-              {hoveredTech}
-            </h2>
-            <p className='text-2xl md:text-3xl font-bold leading-tight'>
-              I use for building modern web applications
-            </p>
-          </motion.div>
+        {/* Title */}
+        <div className='col-span-4 md:col-span-2 bg-background p-8 flex flex-col border justify-center'>
+          <h2 className='text-2xl md:text-3xl font-bold leading-tight text-primary'>
+            {hoveredTech}
+          </h2>
+          <p className='text-2xl md:text-3xl font-bold leading-tight'>
+            I use for building modern web applications
+          </p>
         </div>
 
-        {/* Технологии */}
+        {/* Technologies */}
         {technologies.map((tech, index) => (
-          <motion.div
+          <div
             key={index}
             className={`relative bg-background aspect-square flex items-center justify-center group border-gray-800 ${
               index >= 8 ? 'hidden md:flex' : 'flex'
@@ -48,7 +42,7 @@ export default function Technologies() {
             <div className='relative w-10 h-10 md:w-14 md:h-14'>
               <Image src={tech.icon} alt={tech.name} fill className='object-contain dark:invert' />
             </div>
-          </motion.div>
+          </div>
         ))}
       </div>
     </section>
