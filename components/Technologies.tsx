97735fc4bd7224d1792b44de8bda29b20cd8a2b5
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2025-03-08 14:54:56 +0100

    Refactor Technologies component for responsive text sizing and improved layout

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -9,12 +9,13 @@ export default function Technologies() {
     <section className='container mx-auto px-4 py-20'>
       <div className='grid grid-cols-4 md:grid-cols-6 gap-[1px] border border-gray-800 bg-gray-800'>
         {/* Title */}
-        <div className='col-span-4 md:col-span-2 bg-background p-8 flex flex-col border justify-center'>
-          <h2 className='text-2xl md:text-3xl font-bold leading-tight gradient-text'>
+        <div className='col-span-4 md:col-span-2 bg-background p-[clamp(1rem,2vw,2rem)] flex flex-col border justify-center'>
+          <h2 className='text-2xl md:text-[clamp(1rem,2.5vw,2.5rem)] font-bold leading-tight gradient-text'>
             {hoveredTech}
           </h2>
-          <p className='text-2xl md:text-3xl font-bold leading-tight'>
-            I use for building modern web applications
+          <p className='text-2xl md:text-[clamp(1rem,1.5vw,2rem)] font-bold leading-tight'>
+            I use for building modern <br />
+            web applications
           </p>
         </div>
 
